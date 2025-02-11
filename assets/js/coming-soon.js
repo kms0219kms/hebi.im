dcbbe1e78fe722e94d93f630f89499c927dc787c
kms0219kms <kms0219kms@naver.com>
2025-02-11 19:18:34 +0700

    :bento: feat: After all, I knew you'd be back.

---
+++ assets/js/coming-soon.js
@@ -0,0 +1,17 @@
+/**
+ * Hebi.im - Coming Soon Page_Javascript (coming-soon.html)
+ *
+ * Created by: Minsu Kim <minsu.kim@lunaiz.com> on 2025-02-11
+ * Updated by: Minsu Kim <minsu.kim@lunaiz.com> on 2025-02-11
+ */
+
+const $hero = document.querySelector('#hero');
+
+// Change $hero locale every 10s with fade-in-out effect
+setInterval(() => { 
+    const $currentLocale = $hero.querySelector('div.active');
+    const $nextLocale = $currentLocale.nextElementSibling || $hero.firstElementChild;
+
+    $currentLocale.classList.remove('active');
+    $nextLocale.classList.add('active');
+}, 10000);
