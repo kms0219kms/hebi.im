62acdc287402c705372a4b8fd55998a94054d4cc
kms0219kms <kms0219kms@naver.com>
2025-02-11 20:41:55 +0700

    🍱 feat: After all, I knew you'd be back.
    **UPDATES**
    1. Improve SEO by adding Verification tags
    2. Remove Nympha and replace with DM Serif
    3. Change text-align to right
    4. Change the layout of the youtube link
    5. Change icon to Youtube logo
    6. CSS code spliting
    7. Add blur animation when accessing the page
    8. Fix J/K language's word break rule

--- assets/js/coming-soon.js
+++ assets/js/coming-soon.js
@@ -1,11 +1,11 @@
 /**
- * Hebi.im - Coming Soon Page_Javascript (coming-soon.html)
+ * Hebi.im - Coming Soon Page_Javascript (coming-soon.js)
  *
  * Created by: Minsu Kim <minsu.kim@lunaiz.com> on 2025-02-11
  * Updated by: Minsu Kim <minsu.kim@lunaiz.com> on 2025-02-11
  */
 
-const $hero = document.querySelector('#hero');
+const $hero = document.querySelector('#hero a');
 
 // Change $hero locale every 10s with fade-in-out effect
 setInterval(() => { 
