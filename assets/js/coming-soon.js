a8ac7438997629c39de2fd72d7bd849cdf77681b
kms0219kms <kms0219kms@naver.com>
2025-02-11 23:00:00 +0700

    🍱 feat: After all, I knew you'd be back.
    **UPDATES**
    1. Allowed multiple banner images to switch between them

--- assets/js/coming-soon.js
+++ assets/js/coming-soon.js
@@ -6,6 +6,15 @@
  */
 
 const $hero = document.querySelector('#hero a');
+const $banner = document.querySelector('#hero .mask_banner');
+
+// 랜덤으로 배너 이미지 선택 (1 / $bannerList.length * 100 확률)
+document.addEventListener('DOMContentLoaded', () => {
+    const $bannerList = $banner.querySelectorAll('img');
+
+    const randomIndex = Math.floor(Math.random() * $bannerList.length);
+    $bannerList[randomIndex].classList.add('active');
+});
 
 // Change $hero locale every 10s with fade-in-out effect
 setInterval(() => { 
@@ -15,3 +24,12 @@ setInterval(() => {
     $currentLocale.classList.remove('active');
     $nextLocale.classList.add('active');
 }, 10000);
+
+// Change $bg every 30s with fade-in-out effect
+setInterval(() => { 
+    const $currentBanner = $banner.querySelector('img.active');
+    const $nextBanner = $currentBanner.nextElementSibling || $banner.firstElementChild;
+
+    $currentBanner.classList.remove('active');
+    $nextBanner.classList.add('active');
+}, 30000);
