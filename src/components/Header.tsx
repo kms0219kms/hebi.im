c8c48e0bd3fc2341af6cc48def9b87ae1901e87c
Minsu Kim <kms0219kms@naver.com>
2025-02-17 00:53:28 +0700

    :fire: feat: Navbar 숨기기

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,14 +24,14 @@ export default function Header() {
         </a>
       </div>
 
-      <nav>
+      {/* <nav>
         <a href="/" class={url == "/" && "active"}>
           Home
         </a>
         <a href="/404" class={url == "/404" && "active"}>
           404
         </a>
-      </nav>
+      </nav> */
     </StyledHeader>
   );
 }
