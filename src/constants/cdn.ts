450bea1578f374e0163644ec2d564ae9d025614a
Minsu Kim <kms0219kms@naver.com>
2025-02-17 00:52:17 +0700

    :bug: fix: 제대로 production인지 감지 못함

--- src/constants/cdn.ts
+++ src/constants/cdn.ts
@@ -1,4 +1,4 @@
-export const SHOULD_USE_CDN = import.meta.env.NODE_ENV === "production";
+export const SHOULD_USE_CDN = import.meta.env.PROD;
 
 export const LOCAL_IMAGE_URL = "/assets/images";
 export const IMAGE_CDN_URL = "https://cdn.devayaan.me/hebi-im-images";
